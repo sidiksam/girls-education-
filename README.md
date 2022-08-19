2b01fc055acb491df69d455b09421fa4e40c8a3d
sidiksam <samuraabubakarr03@gmail.com>
2022-08-19 00:57:05 +0000

    update

--- README.md
+++ README.md
@@ -1,1 +1,86 @@
-# girls-education-
+# girl-child-education
+
+Girl child education in sierra leone summit 2022
+
+![](https://img.shields.io/badge/Microverse-blueviolet)
+
+> This project is developed as a capstone project to put to test all we have learnt during the module. It is a Girl child education organization that creates awareness for an up-coming conference for girls and women. This project is done in fulfillment to the requirements Microverse has made.
+
+## Description
+
+## Hosting
+
+This final capstone project hosted on GitHub Page. [Click here](https://sidiksam.github.io/girl-child-education/public/index)
+
+### Mobile version Screenshot:
+
+### Desktop version Screenshot:
+
+
+Here is a [Zoon video that shows the description of the project's features](https://drive.google.com/drive/u/1/folders/1w805Zq4DCGo4k-NB3_q564S1daCRiYWA)
+
+### Objective
+
+This project's goals are as follows:
+
+- Use semantic HTML tags.
+- Apply best practices in HTML code.
+- Use CSS selectors correctly.
+- Use CSS box model.
+- Use Flexbox to place elements in the page.
+- Demonstrate ability to create UIs adaptable to different screen sizes using media queries and css framework(tailwind).
+- Use GitHub Pages to deploy web pages.
+- Apply JavaScript best practices and language style guides in code.
+- Use JavaScript to manipulate DOM elements.
+- Use JavaScript events.
+- Use objects to store and access data.
+- Communicate technical concepts to other technical people.
+
+## Built With
+
+- HTML, CSS (Tailwind ), Javascript
+
+## Getting Started
+
+This is a simple website that creates awareness for a conference for girls non-profit organization. This follow the design according to Cindy Shin in Behance. Do well to give a star ⭐️ as we progress on a journey to becoming better.
+
+### Prerequisites
+
+Basic prequisites for opening a webpage as is required.
+
+- A standard PC
+- Compatible and up to data browser both for PC and mobile.
+- Installed Code Editor
+
+### Usage
+
+This code can be used for educational and code viewing purposed. The copying of these code for other use would be liable to plagarism and practically and developmentally unaccceptable.
+
+## Authors
+
+👤 **Abu Bakarr Samura**
+
+- GitHub: [@sidiksam](https://github.com/sidiksam)
+- Twitter: [@SidikSamura](https://twitter.com/SidikSamura)
+- LinkedIn: [Abu Bakarr Samura](https://www.linkedin.com/in/abu-bakarr-samura-749753241/)
+
+## 🤝 Contributing
+
+Contributions, issues, and feature requests are welcome!
+
+If you find any bug or suggestion for improvement please Feel free to check the [issues page](../../issues/) with your proposed change.
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+
+-Hat tip to ![](https://img.shields.io/badge/Microverse-blueviolet) is where you Gain skills you need to get hired. Learn Online and connect to life changing opportunities. [Apply today](https://www.microverse.org/?grsf=uv064g)
+Design acknowledgment goes to ###Cindy Shin in Behance [Click here to see design](https://www.behance.net/gallery/29845175/CC-Global-Summit-2015)
+
+## Update info
+
+## 📝 License
+
+This project is [MIT](./MIT.md) licensed.
---
+++ src/script.js
@@ -0,0 +1,90 @@
+const hamburgerMenu = document.querySelector('.hamburgerMenu');
+const ulMenu = document.querySelector('.ulMenu');
+const liLink = document.querySelectorAll('.liLink');
+
+function mobileMenu() {
+  hamburgerMenu.classList.toggle('active');
+  ulMenu.classList.toggle('active');
+}
+hamburgerMenu.addEventListener('click', mobileMenu);
+
+function closeMenu() {
+  hamburgerMenu.classList.remove('active');
+  ulMenu.classList.remove('active');
+}
+
+liLink.forEach((n) => n.addEventListener('click', closeMenu));
+
+const guests = {
+  guest1: {
+    id: 1,
+    name: 'Fawaz Milton',
+    title: 'CEO and Director of Girl Child Education Organization',
+    about: 'Fawaz discovered The Girl pinin 4 beteh organization in 2015. He studied Applied Accounting in the Institute of Public Administration and Management(IPAM) University.',
+    checkersImage: '../images/checkerBackground.png',
+    guestdImage: '../images/fawaz.jpg',
+  },
+  guest4: {
+    id: 4,
+    name: 'Sunkray Sunkarie',
+    title: 'Founder and the CEO of the Sunkarie Foundation',
+    about: 'She is a student of Frobay College (FBC)',
+    checkersImage: '../images/checkerBackground.png',
+    guestdImage: '../images/sunkari-foundation_n.jpg',
+  },
+  guest3: {
+    id: 3,
+    name: 'Santos Kamara',
+    title: 'Founder and CEO of My Mother Choice Foundation',
+    about: 'Student at Institute of Public Administration and Management(IPAM) University studying Applied Accounting ',
+    checkersImage: '../images/checkerBackground.png',
+    guestdImage: '../images/santos.jpg',
+  },
+  guest2: {
+    id: 2,
+    name: 'Fatima Bio',
+    title: 'Founder of the women free education',
+    about: 'She has a Bachelor of Arts degree in journalism at the University of the Arts, London College of Communication,',
+    checkersImage: '../images/checkerBackground.png',
+    guestdImage: '../images/fatima.jpg',
+  },
+  guest5: {
+    id: 5,
+    name: 'Idrissa Bangura ',
+    title: 'Co-founder. of the Kids happiness foundation',
+    about: 'Studied social works in University of Milton margai. Currently the CEO of kids happiness foundation in Sierra Leone .',
+    checkersImage: '../images/checkerBackground.png',
+    guestdImage: '../images/alabama.jpg',
+  },
+  speaker6: {
+    id: 6,
+    name: 'Saibatu Mansaray',
+    title: 'Founder and CEO. The Saibatu Mansaray Journey',
+    about: 'A retired milliatery woman in the united state of america (USA).',
+    checkersImage: '../images/checkerBackground.png',
+    guestdImage: '../images/saibatu-foundation.jpg',
+  },
+
+};
+
+const guestsection = document.querySelector('.guestSpeakers');
+
+Object.keys(guests).forEach((guest) => {
+  if (Object.hasOwnProperty.call(guests, guest)) {
+    const speaker = guests[guest];
+    guestsection.innerHTML += `
+        <li class="w-full flex justify-around mobile-speakers">
+        <div class="speakerImg w-44 h-40 relative">
+          <img class="checkers" src='${speaker.checkersImage}' alt=${speaker.about}/>
+          <img class="speakers" src='${speaker.guestdImage}' alt=${speaker.about}/>
+      </div>
+        <div class="speakerAbt flex flex-col  w-2/3">
+          <p class="text-gray-700 md:text-base py-1">${speaker.name}</p>
+      <p class="text-red-700 md:text-base italic py-1 ">${speaker.title}</p>
+          <p class="bg-gray-700 w-12 h-0.5 mb-1"></p>
+          <p class="text-gray-700 md:text-sm">${speaker.about}</p>
+        </div>
+      </li>
+        `;
+  }
+});
